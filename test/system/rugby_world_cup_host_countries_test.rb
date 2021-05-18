592512f7472fcd87bbcc57ff2237e230148ca9ec
Kazuya-Sakashita <kz0508@gmail.com>
2021-05-18 19:37:13 +0900

    グラフ作成

---
+++ test/system/rugby_world_cup_host_countries_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class RugbyWorldCupHostCountriesTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit rugby_world_cup_host_countries_url
+  #
+  #   assert_selector "h1", text: "RugbyWorldCupHostCountry"
+  # end
+end
